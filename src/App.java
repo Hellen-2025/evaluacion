bb3a889eefe51e3684fc178c2f609fee8472bf91
Hellen-2025 <hellenramirez2025@gmail.com>
2025-05-10 11:17:22 -0500

    Se crea proyecto con condicionales simples denominado numeroPar

---
+++ src/App.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner (System.in);
+        System.out.println("Ingrese número");
+        String numeroPar = sc. nextLine();
+
+        int numero = Integer.parseInt (NumeroPar);
+
+        if (Numero %2 == 0 ){
+            System.out.println("El número es par");
+        }
+
+
+    }
+}
